5599125027c3f09cb81b15de8887e446e5496d6d
polinka <zagigalka_60@mail.ru>
2021-03-15 15:30:57 +0300

    smth

--- include/enumeration.hpp
+++ include/enumeration.hpp
@@ -22,9 +22,7 @@
 bool work = true;
 std::vector<nlohmann::json> json_value;
 nlohmann::json j;
-struct data{
-   std::string filename;
-}
+const static std::string filename;
 
 void enumeration() {
   const std::string null_string = "0000";
@@ -60,7 +58,7 @@ void enumeration() {
 
 void create_file_json(){
   std::ofstream file_json;
-  file_json.open(data + ".json");
+  file_json.open(filename + ".json");
   for (unsigned i = 0; i < json_value.size(); ++i){
     file_json << json_value[i].dump(4) << "\n";
   }
