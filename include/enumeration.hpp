d000bbd7e9581920647c528789d75134988a3b43
polinka <zagigalka_60@mail.ru>
2021-03-15 15:42:07 +0300

    smth

--- include/enumeration.hpp
+++ include/enumeration.hpp
@@ -22,7 +22,7 @@
 bool work = true;
 std::vector<nlohmann::json> json_value;
 nlohmann::json j;
-static const std::string filename;
+static std::string filename;
 
 void enumeration() {
   const std::string null_string = "0000";
